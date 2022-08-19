d8abc19ba852be92555765ec3ad03c02c5b43c50
Caroline Ning <ningxueqi1609@163.com>
2022-08-19 17:08:37 -0400

    update

--- app/config/config.php
+++ app/config/config.php
@@ -1,20 +1,18 @@
 <?php
-    // DB params
-    define('DB_HOST','localhost');
-    define('DB_USER','root');
-    define('DB_PASS','');
-    define('DB_NAME','eventeny');
+// DB params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'eventeny');
 
-    /* App root
+/* App root
     __FILE__ : /opt/lampp/htdocs/eventeny/app/config/config.php
     what we want: the app root: /opt/lampp/htdocs/eventeny/app
     dirname - remove the last file. We want to remove two files */
-    define('APPROOT',dirname(dirname(__FILE__)));
+define('APPROOT', dirname(dirname(__FILE__)));
 
-    // URL root
-    define('URLROOT','http://localhost:8080/eventeny');
+// URL root
+define('URLROOT', 'https://eventeny-discount-code.herokuapp.com/');
 
-    // Site Name
-    define('SITENAME','Eventeny');
-
-    
+// Site Name
+define('SITENAME', 'Eventeny');
--- public/index.php
+++ public/index.php
@@ -4,8 +4,3 @@ require_once '../app/bootstrap.php';
 
 // init core library
 $init = new Core;
-
-error_reporting(E_ALL & E_STRICT);
-ini_set('display_errors', '1');
-ini_set('log_errors', '0');
-ini_set('error_log', './');
